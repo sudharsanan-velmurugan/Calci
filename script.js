65d0b86a94607a4dae8cb32d0d7b17d0726df605
sudharsanan <sudharsananv.19eie@kongu.edu>
2022-12-19 22:34:32 +0530

    hello just

---
+++ script.js
@@ -0,0 +1,46 @@
+
+let display=document.getElementById("display")
+
+let add = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let addition= firstvalue+ secondvalue
+    display.innerText=addition
+
+}
+let sub = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let subraction= firstvalue - secondvalue
+    display.innerText=subraction
+
+}
+let mul = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let multiply= firstvalue * secondvalue
+    display.innerText=multiply
+
+}
+let div = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let divition= firstvalue / secondvalue
+    display.innerText=divition
+
+}
+let mod = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let modulo= firstvalue % secondvalue
+    display.innerText=modulo
+
+}
+let pow = ()=> {
+    let firstvalue=Number(document.getElementById("firstvalue").value)
+    let secondvalue=Number(document.getElementById("secondvalue").value)
+    let power=  Math.pow(firstvalue,secondvalue) 
+    display.innerText=power
+
+}
+
